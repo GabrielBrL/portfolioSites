d67861ef569ce21f358edf86956860a9d22b72aa
DarkBarros <gabrielnois7@gmail.com>
2021-08-12 23:41:23 -0300

    Primeiro site modelo para o portfolio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle');
+    const sideBar = document.querySelector('.sideBar');
+    menuToggle.classList.toggle('active');
+    sideBar.classList.toggle('active')
+}
